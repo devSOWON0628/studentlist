6fff136cfd8e27e6056cccae7160d6790b763a71
소원 박 <s2018s29@e-mirim.hs.kr>
2020-05-19 15:35:16 +0900

    class, list 삭제

---
+++ studentlist/Student.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentlist
+{
+    class Student
+    {
+        public string id;
+        public string name;
+        public int grade;
+        public string major;
+        public DateTime birthday;
+    }
+}
